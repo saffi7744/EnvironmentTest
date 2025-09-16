b8307aed9861529dbd0ba331416b7abce135e325
saffi7744 <saffigithub@gmail.com>
2025-09-16 19:14:20 -0400

    Update App.js

--- App.js
+++ App.js
@@ -17,7 +17,7 @@ return (
 <Text style={styles.subtitle}>Cross-Platform Mobile Development</Text>
 </View>
 <View style={styles.content}>
-//Greeting
+// Greeting
 <Text style={styles.greeting}>Hello, I'm Saffi Ahmad!</Text>
 <Text style={styles.info}>Student ID: n01708324@humber.ca</Text>
 <Text style={styles.info}>Program: Computer Programming & Analysis</Text>
@@ -32,7 +32,7 @@ return (
 </SafeAreaView>
 );
 }
-//Styling
+// Styling
 const styles = StyleSheet.create({
 container: {
 flex: 1,
